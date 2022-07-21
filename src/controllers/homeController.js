7f3512c97a0595bf21f020cd5675d09de2d9f3a0
Georgi Petrov <hailanimepenchou@gmail.com>
2022-07-21 22:02:54 +0300

    added home greeting for logged user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const jwtServices = require('../services/jwtServices');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('home');
+    const token = req.cookies.sessionToken;
+
+    const result = jwtServices.decodeToken(token);
+    if (result) {
+        res.render('home', { email: result.email });
+    } else {
+        res.render('home', { email: 'Guest' });
+    }
+    
 });
 
 module.exports = router;
