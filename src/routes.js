3491171e6750a1ce3e26b8ac7f6c7b3ea896806a
Georgi Petrov <hailanimepenchou@gmail.com>
2022-07-21 16:58:16 +0300

    added routing to login and register

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const homeController = require('./controllers/homeController');
+const authController = require('./controllers/authController');
 
 const router = express.Router();
 
 router.use('/', homeController);
+router.use('/auth', authController);
 
 module.exports = router;
